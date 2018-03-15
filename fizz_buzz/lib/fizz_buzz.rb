51ab759f6314d39c9b5bffc9f5e4c797f8928a6d
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-03-15 20:15:10 +0100

    Refactor fizz_buss.rb

--- fizz_buzz/lib/fizz_buzz.rb
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -1,13 +1,17 @@
 def fizz_buzz(number)
   if number < 1 || number > 100 || number == !Integer
     'pick a number from 1 to 100'
-  elsif number % 15 == 0
+  elsif has_zero_remainder?(number, 15)
     'fizz buzz'
-  elsif number % 5 == 0
+  elsif has_zero_remainder?(number, 5)
     'buzz'
-  elsif number % 3 == 0
+  elsif has_zero_remainder?(number, 3)
     'fizz'
   else
     number
    end
+end
+
+def has_zero_remainder?(number, divider)
+  number % divider == 0
 end
