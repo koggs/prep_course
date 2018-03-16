d87e0debd0e7f1dd3c0dae1435e5ba39c606473a
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-03-16 21:44:23 +0100

    finished exercise 1

---
+++ Javascript_Exercises/javaskript_1.js
@@ -0,0 +1,23 @@
+//Question 1.
+var myArray = [1, 2, 3];
+
+//Question 2.
+myArray[1];
+
+//Question 3.
+//123/12 are floats,
+//"Things in quotes" is a string,
+//undefined is a variable with out value.
+
+//Question 4.
+if (10 > 9) {
+  true  };
+  else {
+  false  };
+
+  //Question 5.
+  var myArray = ["Thomas", "Amber", "Raoul"];
+  var emptyArray = [ ];
+  for(i = 0; i < myArray.length; i++) {
+  emptyArray.push(myArray[i]);
+}
